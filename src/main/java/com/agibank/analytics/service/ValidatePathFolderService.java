196b9605665404053229534fc5483f2aac0e6f2e
Alysson Zanette <alysson.zanette@softplan.com.br>
2020-07-13 17:59:14 -0300

    Removed unused property

--- src/main/java/com/agibank/analytics/service/ValidatePathFolderService.java
+++ src/main/java/com/agibank/analytics/service/ValidatePathFolderService.java
@@ -8,8 +8,6 @@ import java.nio.file.Path;
 @Service
 public class ValidatePathFolderService {
 
-  private static final String BAR = "/";
-
   public void validatePathFolder(Path path) {
     var file = new File(path.toString());
     if (!file.exists()) {
