3932be61424c3efab67fa9a6d1158d60fb4d8798
Alysson Zanette <alysson.zanette@softplan.com.br>
2020-07-13 17:44:10 -0300

    Fix package name controller

---
+++ src/main/java/com/agibank/analytics/controller/HandleException.java
@@ -0,0 +1,16 @@
+package com.agibank.analytics.controller;
+
+import com.agibank.analytics.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class HandleException {
+
+  @ExceptionHandler(NotFoundException.class)
+  public ResponseEntity handleNotFoundExpceiton(NotFoundException e) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+  }
+}
